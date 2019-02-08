6e7991d392d21097c31ebb465913fa97c5d4aa7f
Keith Gross <selkcipehtdna@gmail.com>
2019-02-07 22:00:53 -0800

    @cisc190mesa/cisc190 update on 2/7/2019

--- Description.java
+++ Description.java
@@ -1,20 +1,18 @@
+import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 /**
  * Description 
  *
- * @author Keith Gross
- * @version 0.0.1
- * @cisc190mesa/cisc190
  */
 public class Description
 {
     // height in meters.
     private float height; //Primative
     
-    // weight in kilograms
-    private float weight;
+    private float weight; // weight in kilograms
     
-    private EyeColor eyeColor; //Class
+    private EyeColor eyeColor; //Class EyeColor
     
     private Gender gender;
     
@@ -23,10 +21,51 @@ public class Description
     private Date birthDate;
 
     /**
-     * Constructor for objects of class Description
+     * Constructor for objects of class Description, Constructor has same name as class and no return type.
      */
-    public Description()
+    public Description(float height, float weight, EyeColor eyeColor, Gender gender, int year, int month, int day)
     {
-       
+       this.height = height;
+       this.weight = weight;
+       this.eyeColor = eyeColor;
+       this.gender = gender;
+       Calendar bday = new Calendar.Builder() // Builder is a public static Class nested inside the Calendar Class
+            .setCalendarType("iso8601")
+            .setDate(year, month, day)
+            .build();
+            // convert the calendar to a date
+       this.birthDate = Date.from(bday.toInstant());
+    }
+    
+    public float getHeightMetric()
+    {
+        return height;
+    }
+    
+    public float getHeightImperial()
+    {
+        //TODO: convert to feet/inches
+        return 0.0F;
+    }
+    
+    public float getWeightImperial()
+    {
+        //TODO: Convert to pounds/oz
+        return 0.0F;
+    }
+    
+    public EyeColor getEyeColor()
+    {
+        return eyeColor;
+    }
+    
+    public Gender getGender()
+    {
+        return gender;
+    }
+    
+    public Date getBirthDate()
+    {
+        return birthDate;
     }
 }
